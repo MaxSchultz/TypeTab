492128100e84cb813cb8923a9cf8c898519efa7f
MaxSchultz <max.schultz@gmail.com>
2013-10-17 11:28:16 -0700

    GitHub Page yo

---
+++ app.js
@@ -0,0 +1,11 @@
+//INFO MENU
+$(document).ready(function ($) {
+ $('.info-btn').pageslide({ direction: "left" });
+  $("h1").lettering();
+ $('#info-close').click(function(){
+  $.pageslide.close();
+});
+
+   
+
+});
